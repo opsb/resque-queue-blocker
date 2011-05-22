06e3ff636b1a6c0838e64b2da87aa706d3a52e31
opsb <oliver@opsb.co.uk>
2011-05-23 00:18:46 +0100

    Add config for redis to go

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require 'resque'
 require 'resque_queue_blocker'
 require 'ruby-debug'
 
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 RSpec.configure do |config|
   config.after(:each) do
     Resque.redis.flushdb    
